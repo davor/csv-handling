f22e1b460c444d5736fe416ff8bcca4922274e48
David Vorgrimmler <vorgrimmlerdavid@gmx.de>
2012-07-23 12:48:52 +0200

    Fixed bugs and added File name addition.

--- csv_handling.sh
+++ csv_handling.sh
@@ -12,6 +12,8 @@ get_column() {
   local COLUMN=$2
   cut -d, -f$COLUMN < "$FILE"
 }
+#get_column a.csv 1
+
 
 # Returns n-th row of file 
 # @param1 path/to/csv_file.csv
@@ -23,6 +25,7 @@ get_row() {
   local ROW=$2
   sed -n "$ROW"p < "$FILE"
 }
+#get_row a.csv 1
 
 # Merge files (attach first line file1 with first line file2 ...)
 # @param1 path/to/csv_file_1.csv
@@ -32,14 +35,15 @@ get_row() {
 # @example merge_columns_file a.csv b.csv
 merge_columns_file() {
   local FILES="$*"
-  paste -d , "$FILES"
+  paste -d , $FILES
 }
+#merge_columns_file a.csv b.csv
 
 # Displays number of columns for file with n X m columns
 # @param1 path/to/csv_file_1.csv
 # @param2 separator (e.g. ",")
 # @return Sets variable $COLCNT to number of columns 
-# @example colcnt , a.csv; echo $COLCNT
+# @example colcnt a.csv "," ; echo $COLCNT
 colcnt() {
   local FILE="$1";
   local DELIM="$2";
@@ -62,6 +66,23 @@ colcnt() {
     return 1
   fi
 }
+#colcnt a.csv "," ; echo $COLCNT
+
+# Add headline(string) to file
+# @param1 path/to/csv_file.csv
+# @param2 String
+# @return Inserts String at top of csv_file
+# @example add_headline2file abcd.txt bla,bla,blub 
+add_headline2file() {
+  local FILE="$1"
+  local HL="$2"
+  echo "$FILE"
+  echo "$HL" > "$FILE".tmp
+  cat "$FILE" >> "$FILE".tmp
+  mv "$FILE".tmp "$FILE"
+}
+#add_headline2file abcd.txt bla,bla,blub 
+
 
 # Merge columns of different files and create new files for each column
 # @param1 path/to/csv_file_1.csv
@@ -73,9 +94,9 @@ get_separated_columns() {
   local FILES="$*"
   local NR_FILES=$#
   local COLS=0
-  for F in "$FILES"  
+  for F in $FILES  
   do
-    colcnt , "$F"
+    colcnt "$F" ,
     if [ $? -eq 0 ] ; then
       if [ $COLS -eq 0 ] ; then
         COLS=$COLCNT
@@ -95,27 +116,19 @@ get_separated_columns() {
   while [ $cnt1 -le $COLS ] 
   do
     local cnt2=1
-    for f in "$FILES"
+    for f in $FILES
     do
       get_column "$f" $cnt1 > get_separated_columns_tmp_file_"$cnt1"_"$cnt2".tmp
-      cnt2=$(($cnt2+1))
+      
     done
-    merge_columns_file get_separated_columns_tmp_file_"$cnt1"_*.tmp > column_"$cnt1"_merged.csv
+    NAME=""
+    #NAME="MCC_"
+    merge_columns_file get_separated_columns_tmp_file_"$cnt1"_*.tmp > "$NAME"column_"$cnt1"_merged.csv
+    #add_headline2file "$NAME"column_"$cnt1"_merged.csv MLE,MEAN,BBRC
     rm get_separated_columns_tmp_file_*.tmp
     cnt1=$(($cnt1+1))
   done
 }
+get_separated_columns a.csv b.csv c.csv
+#get_separated_columns ../exp3/bbrc_sample_MCC*
 
-# Add headline(string) to file
-# @param1 path/to/csv_file.csv
-# @param2 String
-# @return Inserts String at top of csv_file
-# @example add_headline2file abcd.txt bla,bla,blub 
-add_headline2file() {
-  local FILE="$1"
-  local HL="$2"
-  echo "$FILE"
-  echo "$HL" > "$FILE".tmp
-  cat "$FILE" >> "$FILE".tmp
-  mv "$FILE".tmp "$FILE"
-}
